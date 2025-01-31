7195848ca3512b333a3215b9f40da62e75b44b15
Kimarlee <kimarlee.tran@gmail.com>
2025-01-30 19:19:11 -0600

    Added more python code

---
+++ practice.py
@@ -0,0 +1,15 @@
+# The list of candies to print to the screen
+candy_list = ["Snickers", "Kit Kat", "Sour Patch Kids", "Juicy Fruit", "Swedish Fish", "Skittles", "Hershey Bar", "Starbursts", "M&Ms"]
+
+# The amount of candy the user will be allowed to choose
+allowance = 5
+
+# The list used to store all of the candies selected inside of
+candy_cart = []
+
+# Print out options
+for candy in candy_list:
+        print(f'[{(candy_list.index(candy))}] {candy}')
+for x in range(allowance):
+    selected = input("Input the number of the candy you want: ")
+    print("Here are the candies you chose " + str(selected))
